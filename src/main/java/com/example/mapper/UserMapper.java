f7bab2729ad1e27fb2e111c46a9f6f6d30f35ac6
love <3489896841@qq.com>
2026-04-21 22:02:23 +0800

    feat: add user mapper

---
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.example.mapper;
+
+import com.example.entity.User;
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+public interface UserMapper {
+    @Select("SELECT * FROM t_user")
+    List<User> findAll();
+
+    @Select("SELECT * FROM t_user WHERE id = #{id}")
+    User findById(Integer id);
+
+    @Insert("INSERT INTO t_user(name, age, email) VALUES(#{name}, #{age}, #{email})")
+    int add(User user);
+
+    @Update("UPDATE t_user SET name=#{name}, age=#{age}, email=#{email} WHERE id=#{id}")
+    int update(User user);
+
+    @Delete("DELETE FROM t_user WHERE id=#{id}")
+    int deleteById(Integer id);
+
+    @Select("SELECT * FROM t_user WHERE name LIKE CONCAT('%',#{name},'%')")
+    List<User> findByName(String name);
+}
