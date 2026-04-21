e3c56a17a0d034eb4bf3edd5f3c7c0d5d1867d82
love <3489896841@qq.com>
2026-04-21 22:04:01 +0800

    feat: add create user servlet

---
+++ src/main/java/com/example/servlet/AddServlet.java
@@ -0,0 +1,43 @@
+package com.example.servlet;
+
+import com.example.entity.User;
+import com.example.mapper.UserMapper;
+import com.example.util.MyBatisUtil;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.ibatis.session.SqlSession;
+
+@WebServlet(value = "/add")
+public class AddServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String name = request.getParameter("name");
+        Integer age = Integer.parseInt(request.getParameter("age"));
+        String email = request.getParameter("email");
+
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        user.setEmail(email);
+
+        try (SqlSession sqlSession = MyBatisUtil.getSqlSession()) {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            mapper.add(user);
+        }
+        response.sendRedirect("list");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/add.jsp").forward((ServletRequest) request, (ServletResponse) response);
+    }
+}
