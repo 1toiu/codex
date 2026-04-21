902fc4d4d220126cd493a4989b570afdb4be66a0
love <3489896841@qq.com>
2026-04-21 22:03:09 +0800

    feat: add admin permission filter

---
+++ src/main/java/com/example/filter/AdminFilter.java
@@ -0,0 +1,37 @@
+package com.example.filter;
+
+import com.example.auth.AuthUtil;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(filterName = "AdminFilter", urlPatterns = {"/add", "/edit", "/update", "/delete", "/add.jsp", "/edit.jsp"})
+public class AdminFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if (!AuthUtil.isLoggedIn(request)) {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return;
+        }
+
+        if (AuthUtil.isAdmin(request)) {
+            chain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute("flash", "普通用户只能查看列表，不能新增、编辑或删除。");
+        response.sendRedirect(request.getContextPath() + "/list");
+    }
+}
