8fa50bb652f9acdc8994db4294371736c3ddd1d7
love <3489896841@qq.com>
2026-04-21 22:04:08 +0800

    feat: add update user servlet

---
+++ src/main/java/com/example/servlet/UpdateServlet.java
@@ -0,0 +1,38 @@
+package com.example.servlet;
+
+import com.example.entity.User;
+import com.example.mapper.UserMapper;
+import com.example.util.MyBatisUtil;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.ibatis.session.SqlSession;
+
+@WebServlet(value = "/update")
+public class UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        Integer age = Integer.parseInt(request.getParameter("age"));
+        String email = request.getParameter("email");
+
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setAge(age);
+        user.setEmail(email);
+
+        try (SqlSession sqlSession = MyBatisUtil.getSqlSession()) {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            mapper.update(user);
+        }
+        response.sendRedirect("list");
+    }
+}
