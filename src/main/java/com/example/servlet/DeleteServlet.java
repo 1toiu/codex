3b810924b4a3665fc26e429b1c64a001e97f163a
love <3489896841@qq.com>
2026-04-21 22:04:57 +0800

    feat: add delete user servlet

---
+++ src/main/java/com/example/servlet/DeleteServlet.java
@@ -0,0 +1,26 @@
+package com.example.servlet;
+
+import com.example.mapper.UserMapper;
+import com.example.util.MyBatisUtil;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.ibatis.session.SqlSession;
+
+@WebServlet(value = "/delete")
+public class DeleteServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        try (SqlSession sqlSession = MyBatisUtil.getSqlSession()) {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            mapper.deleteById(id);
+        }
+        response.sendRedirect("list");
+    }
+}
